27a7586b7523492f985d8eeb630f9c33b6baf15c
Zac <zacaree@gmail.com>
2018-08-06 16:02:28 -0500

    Built an animated sidebar nav with GSAP

--- 001_Five Bouncing Squares/script.js
+++ 001_Five Bouncing Squares/script.js
@@ -1,4 +1,4 @@
-TweenMax.staggerFrom('.box', 1.5, {
+TweenMax.staggerFrom('.square', 1.5, {
   opacity: 0,
   y: -600,
   rotation: 360,
@@ -6,7 +6,7 @@ TweenMax.staggerFrom('.box', 1.5, {
   ease: Bounce.easeOut,
 }, 0.2);
 
-TweenMax.to('.box', .7, {
+TweenMax.to('.square', .7, {
   opacity: 0,
   rotation: 360,
   scale: 0,
---
+++ 002_Staggering Menu/script.js
@@ -0,0 +1,48 @@
+
+const btnOpen = document.querySelector('.btnOpen');
+const btnClose = document.querySelector('.btnClose');
+
+
+// ---------------
+
+
+const tl = new TimelineMax({paused: true});
+
+tl.timeScale(1);
+
+tl.to('h1', 0.5, { opacity: 0 })
+
+  .to(btnOpen, 0.5, {
+    x: -300,
+    opacity: 0,
+    ease: Power2.easeOut,
+  }, '-=0.5')
+
+  .to('ul', 0.5, {
+    x: 0,
+    ease: Power2.easeOut,
+  }, '-=0.5')
+
+  .to(btnClose, 0.5, {
+    x: 0,
+    opacity: 1,
+    rotation: 360,
+    ease: Power1.easeOut,
+  }, '-=0.5')
+
+
+  .staggerFrom('li', 0.2, {
+    opacity: 0,
+    x: 70,
+    ease: Back.easeOut,
+  }, 0.1, '-=0.4');
+
+
+// ---------------
+
+
+openMenu = () => tl.play();
+closeMenu = () => tl.reverse();
+
+btnOpen.addEventListener('click', openMenu, false);
+btnClose.addEventListener('click', closeMenu, false);
